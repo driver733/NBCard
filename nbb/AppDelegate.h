cf13e08b87e21d9cc30fd29dd405681277337764
Mike <mikhailyakushin@Mikes-MacBook-Air-2.local>
2014-10-08 18:30:47 -0400

    Initial Commit

---
+++ nbb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nbb
+//
+//  Created by Mike on 10/8/14.
+//  Copyright (c) 2014 Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ nbb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  nbb
+//
+//  Created by Mike on 10/8/14.
+//  Copyright (c) 2014 Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
